85256a44ebdf421cf945cdb107288c5535e89bdc
mateuszbugaj <mateuszbugaj5@gmail.com>
2020-12-25 21:00:40 +0100

    Moved to java 11 / added local dependencies / updated readme / removed missing mtl file warnings / configured maven for terminal launch

--- src/main/java/TwitchRobot.java
+++ src/main/java/TwitchRobot.java
@@ -1,8 +1,12 @@
 import GUI.Canvas;
+import Utils.FileReader;
 import processing.core.PApplet;
 
 
 public class TwitchRobot{
     public static void main(String[] args) {
+        String logo = FileReader.read("src/main/resources/text_files/logo.txt");
+        System.out.println(logo);
+
         PApplet.main(Canvas.class); }
 }
